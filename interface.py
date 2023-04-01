66a03410a4a16dbd3c592970a6dec33c9f8dc716
BuildTools <unconfigured@null.spigotmc.org>
2023-04-01 18:14:48 +0200

    Initial commit

---
+++ interface.py
@@ -0,0 +1,77 @@
+from tkinter import *
+from math import *
+from utilitaries import *
+
+
+class Switch:
+    def __init__(self, root, function_on=f_pass, function_off=f_pass):
+        self.state = False
+        self.function_on, self.function_off = function_on, function_off
+        self.canvas = Canvas(root, bg="#0A062E", relief="flat", highlightbackground="#0A062E", width=30, height=20)
+        self.border = [
+            self.canvas.create_oval(1, 1, 21, 21, fill="#343563", width="0"),
+            self.canvas.create_oval(11, 1, 31, 21, fill="#343563", width="0"),
+            self.canvas.create_rectangle(8, -1, 23, 22, fill="#343563", width="0")
+        ]
+        self.bg = [
+            self.canvas.create_oval(2, 2, 20, 20, fill="#48498A", width="0"),
+            self.canvas.create_oval(12, 2, 30, 20, fill="#48498A", width="0"),
+            self.canvas.create_rectangle(9, 0, 22, 21, fill="#48498A", width="0")
+        ]
+        self.button = self.canvas.create_oval(4, 4, 18, 18, fill="#DDE0F5", width="0")
+        self.canvas.bind("<Button-1>", self.switch)
+
+    def place(self, x, y, anchor="nw"):
+        self.canvas.place(x=x, y=y, anchor=anchor)
+
+    def switch(self, e):
+        self.state = not self.state
+        if self.state:
+            for i in range(20):
+                self.canvas.after(int(i/100*1000), lambda x=-5*cos(i/20*pi)+5*cos((i-1)/20*pi): self.canvas.move(self.button, x, 0))
+                rgb = tuple(((i+1)*hex_to_rgb("#F5C110")[j] + (19-i)*hex_to_rgb("#48498A")[j])//20 for j in range(3))
+                for k in range(3): self.canvas.after(int(i/100*1000), lambda color=rgb_to_hex(rgb), idx=k: self.canvas.itemconfig(self.bg[idx], fill=color))
+                rgb = tuple(((i+1)*hex_to_rgb("#B59644")[j] + (19-i)*hex_to_rgb("#343563")[j])//20 for j in range(3))
+                for k in range(3): self.canvas.after(int(i/100*1000), lambda color=rgb_to_hex(rgb), idx=k: self.canvas.itemconfig(self.border[idx], fill=color))
+            self.function_on()
+        else:
+            for i in range(20):
+                self.canvas.after(int(i/100*1000), lambda x=5*cos(i/20*pi)-5*cos((i-1)/20*pi): self.canvas.move(self.button, x, 0))
+                rgb = tuple(((19-i)*hex_to_rgb("#F5C110")[j] + (i+1)*hex_to_rgb("#48498A")[j])//20 for j in range(3))
+                for k in range(3): self.canvas.after(int(i/100*1000), lambda color=rgb_to_hex(rgb), idx=k: self.canvas.itemconfig(self.bg[idx], fill=color))
+                rgb = tuple(((i+1)*hex_to_rgb("#343563")[j] + (19-i)*hex_to_rgb("#B59644")[j])//20 for j in range(3))
+                for k in range(3): self.canvas.after(int(i/100*1000), lambda color=rgb_to_hex(rgb), idx=k: self.canvas.itemconfig(self.border[idx], fill=color))
+            self.function_off()
+
+    def destroy(self):
+        self.canvas.destroy()
+
+
+class VinylSwitch:
+    def __init__(self, root, function_on=f_pass, function_off=f_pass):
+        self.state = False
+        self.function_on, self.function_off = function_on, function_off
+        self.canvas = Canvas(root, bg="#0A062E", relief="flat", highlightbackground="#0A062E", width=240, height=200)
+        self.canvas.bind("<Button-1>", self.switch)
+        self.canvas.create_oval(-39, 1, 199, 239, fill="#343563")
+        self.canvas.create_oval(-36, 4, 196, 236, fill="black")
+        for i in range(3):
+            self.canvas.create_oval(-20+17*i, 20+17*i, 180-17*i, 220-17*i, fill="#343563")
+            self.canvas.create_oval(-17+17*i, 23+17*i, 177-17*i, 217-17*i, fill="black")
+        self.canvas.create_polygon(80, 120, 80, 10, 0, 40, -24, 180, 80, 120, 80, 240, 153, 203, 187, 126, 174, 66, fill="black")
+        self.canvas.create_oval(55, 95, 105, 145, fill="#F5C110", outline="#B59644")
+        self.canvas.create_oval(77, 117, 83, 123, fill="#0A062E", outline="#B59644")
+
+
+    def place(self, x, y, anchor="nw"):
+        self.canvas.place(x=x, y=y, anchor=anchor)
+
+    def switch(self, e):
+        self.state = not self.state
+        if self.state:
+            self.function_on()
+        else:
+            self.function_off()
+
+    def destroy(self):
+        self.canvas.destroy()
---
+++ main.py
@@ -0,0 +1,89 @@
+from interface import *
+import asyncio
+import discord
+import pyaudio
+from utilitaries import *
+from async_tkinter_loop import async_mainloop, async_handler
+
+
+if __name__ == '__main__':
+
+    window = Tk()
+    window.config(bg="#0A062E", width=800, height=350)
+    window.title("La Platine du turfu")
+    window.resizable(0, 0)
+    vinylswitch = VinylSwitch(window)
+    vinylswitch.place(x=0, y=350, anchor='sw')
+    window.update()
+
+
+    intents = discord.Intents().default()
+    client = discord.Client(intents=intents)
+
+    file = open('C:/Users/cedri/OneDrive/NSI/DiscordPy/vinyles.csv', 'r')
+    data = file.read()
+    file.close()
+    vinyles = data.split('\n')
+
+    config = {}
+    with open("config.ini", "r") as f:
+        for line in f.readlines():
+            key, arg = line[:-1].split('=')
+            config[key] = int(arg) if arg.isdigit() else arg
+
+
+    @client.event
+    async def on_ready():
+        global vc
+        vc = None
+        print(f'\n----------------------\nLogged in as {client.user}\n----------------------\n\n')
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='des vinyles'))
+        connected = False
+        for i in [i for i in client.get_all_channels() if i.type == discord.ChannelType.voice]:
+            # print(config["user_id"], '\n', i.voice_states.keys())
+            if config["owner_id"] in i.voice_states.keys():
+                print(f"Connect to {i}")
+                #vc = await i.connect()
+                connected = True
+                print(f"Connected to {i}")
+                await play_audio_in_voice()
+        if not connected:
+            i = client.get_channel(config["default_channel_id"])
+            print(f"Connect to {i}")
+            vc = await i.connect()
+            print(f"Connected to {i}")
+            await play_audio_in_voice()
+        '''
+        #client.loop.create_task(bg_task())
+        print('\nFaites votre choix:\n')
+        for i in range(len(vinyles)):
+            print(f'{i + 1}. {vinyles[i]}')
+        print('\nSaisissez le numéro du vinyle: ', end='')'''
+
+
+    class PyAudioPCM(discord.AudioSource):
+        def __init__(self, channels=2, rate=48000, chunk=960, input_device=1) -> None:
+            p = pyaudio.PyAudio()
+            self.chunks = chunk
+            self.input_stream = p.open(format=pyaudio.paInt16, channels=channels, rate=rate, input=True,
+                                       input_device_index=input_device, frames_per_buffer=chunk)
+
+        def read(self) -> bytes:
+            return self.input_stream.read(self.chunks)
+
+
+    async def play_audio_in_voice():
+        vc.play(PyAudioPCM(), after=lambda e: print(f'Player error: {e}') if e else None)
+        switch = Switch(window, function_on=vc.resume, function_off=vc.pause)
+        switch.switch(0)
+        switch.place(x=50, y=50)
+
+
+    @async_handler
+    async def client_start():
+        await client.start(config["token"])
+
+
+    window.after(0, client_start)
+
+    async_mainloop(window)
